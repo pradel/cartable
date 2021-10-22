d208674d5b9b9cdf9c65d0d9466a3d75f5e82de4
Pradel Léo <pradel.leo@gmail.com>
2021-10-22 22:22:58 +0200

    feat: add test command

---
+++ packages/cartable-core/src/generateJestConfig.ts
@@ -0,0 +1,39 @@
+import type { Config } from "@jest/types";
+import { existsSync } from "fs";
+import { resolve } from "path";
+
+export const generateJestConfig = (): Config.InitialOptions => {
+  const swcRcPath = resolve(".swcrc");
+  const hasSwcRc = existsSync(swcRcPath);
+  const tsConfigPath = resolve("tsconfig.json");
+  const useTypeScript = existsSync(tsConfigPath);
+
+  let swrOptions;
+
+  if (!hasSwcRc) {
+    swrOptions = {
+      sourceMap: true,
+      jsc: {
+        target: "es2020",
+        parser: !useTypeScript
+          ? {
+              syntax: "ecmascript",
+              jsx: false,
+              dynamicImport: true,
+            }
+          : {
+              syntax: "typescript",
+              tsx: false,
+              dynamicImport: true,
+            },
+      },
+    };
+  }
+
+  return {
+    transform: {
+      "^.+\\.(t|j)sx?$": swrOptions ? ["@swc/jest", swrOptions] : "@swc/jest",
+    },
+    moduleFileExtensions: ["ts", "js", "json"],
+  };
+};
