6bf1fd9b2a2aadcf9ea52ab095f86fd57df90adf
Léo Pradel <pradel.leo@gmail.com>
2021-10-22 22:04:14 +0200

    feat: add custom webpack configuration example (#6)

---
+++ examples/custom-webpack-config/cartable.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  webpack: (config) => {
+    // Perform customizations to config
+    // Important: return the modified config
+
+    // changes the name of the entry point from index -> main.js
+    config.entry.main = ["./services/main.js"];
+
+    return config;
+  },
+};
